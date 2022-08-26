740e6f866a15b9214f0187c10c849fd33626a3d8
ekmc16 <112057282+ekmc16@users.noreply.github.com>
2022-08-26 11:21:05 +0800

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO('mysql:host=us-cdbr-east-06.cleardb.net/;port=3306;dbname=heroku_66aa9a40c1910f5','b959a8e2c39313', 'a9259e0c');
+$pdo = new PDO('mysql:host=us-cdbr-east-06.cleardb.net;port=3306;dbname=heroku_66aa9a40c1910f5','b959a8e2c39313', 'a9259e0c');
 
 // $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=misc','postgres', 'new-password');
 
