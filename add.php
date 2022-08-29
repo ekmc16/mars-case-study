88ec458d61b7f5ead9351b73ecf49f8d0230eb4f
ekmc16 <112057282+ekmc16@users.noreply.github.com>
2022-08-29 17:49:39 +0800

    Update add.php

--- add.php
+++ add.php
@@ -1,44 +1,65 @@
 <?php
 require_once "pdo.php";
 
-// $error='';
-// $success='';
+$error='';
+$success='';
 
-// if(isset($_POST)){
-//   if( ($_POST['first_name']) && isset($_POST['last_name'])
-//       && isset($_POST['base']) && isset($_POST['superior'])) {
-//       // Data validation
-//       if ( strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1) {
-//           $error = 'Missing data';
-//       }
-//       else{
+if(isset($_POST)){
+  if( ($_POST['first_name']) && isset($_POST['last_name'])
+      && isset($_POST['base']) && isset($_POST['superior'])) {
+      // Data validation
+      if ( strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1) {
+          $error = 'Missing data';
+      }
+      else{
+        if(is_null($_POST['base'])){
+          $sql = "INSERT INTO martian (first_name, last_name, super_id)
+                  VALUES (:fname, :lname, :baseid, :superid)";
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute(array(
+              ':fname' => $_POST['first_name'],
+              ':lname' => $_POST['last_name'],
+              ':superid' => $_POST['superior']
+            ));
+
+        }
+        else if(is_null($_POST['superior'])){
+          $sql = "INSERT INTO martian (first_name, last_name, base_id)
+                  VALUES (:fname, :lname, :baseid, :superid)";
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute(array(
+              ':fname' => $_POST['first_name'],
+              ':lname' => $_POST['last_name'],
+              ':baseid' => $_POST['base']
+            ));
+        }
+        else{
         $sql = "INSERT INTO martian (first_name, last_name, base_id, super_id)
                 VALUES (:fname, :lname, :baseid, :superid)";
         $stmt = $pdo->prepare($sql);
-        $a=$stmt->execute(array(
-            ':fname' => 'test',
-            ':lname' => 'test',
-            ':baseid' => '',
-            ':superid' => ''
+        $stmt->execute(array(
+            ':fname' => $_POST['first_name'],
+            ':lname' => $_POST['last_name'],
+            ':baseid' => $_POST['base'],
+            ':superid' => $_POST['superior']
           ));
-        echo $a;
-        // $success = 'Record Added';
-//       }
-//   }
+        }
+        $success = 'Record Added';
+      }
+  }
 
-//   else{
-//     $error = 'ERROR';
-//   }
+  else{
+    $error = 'ERROR';
+  }
 
-//   $data = array(
-//     'error'     =>  $error,
-//     'success'   =>  $success
-//     );
+  $data = array(
+    'error'     =>  $error,
+    'success'   =>  $success
+    );
   
-//   echo json_encode($data);
-// }
+  echo json_encode($data);
+}
 
     
 ?>
 
-heheheheh
