88c1eea9eb52c537e739414224cd3a69148290fa
ekmc16 <112057282+ekmc16@users.noreply.github.com>
2022-08-29 17:15:56 +0800

    Update add.php

--- add.php
+++ add.php
@@ -1,41 +1,41 @@
 <?php
 require_once "pdo.php";
 
-$error='';
-$success='';
+// $error='';
+// $success='';
 
-if(isset($_POST)){
-  if( ($_POST['first_name']) && isset($_POST['last_name'])
-      && isset($_POST['base']) && isset($_POST['superior'])) {
-      // Data validation
-      if ( strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1) {
-          $error = 'Missing data';
-      }
-      else{
+// if(isset($_POST)){
+//   if( ($_POST['first_name']) && isset($_POST['last_name'])
+//       && isset($_POST['base']) && isset($_POST['superior'])) {
+//       // Data validation
+//       if ( strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1) {
+//           $error = 'Missing data';
+//       }
+//       else{
         $sql = "INSERT INTO martian (first_name, last_name, base_id, super_id)
                 VALUES (:fname, :lname, :baseid, :superid)";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(array(
-            ':fname' => $_POST['first_name'],
-            ':lname' => $_POST['last_name'],
-            ':baseid' => $_POST['base'],
-            ':superid' => $_POST['superior']
+            ':fname' => 'test',
+            ':lname' => 'test',
+            ':baseid' => '',
+            ':superid' => ''
           ));
         // $success = 'Record Added';
-      }
-  }
+//       }
+//   }
 
-  else{
-    $error = $_POST['superior'];
-  }
+//   else{
+//     $error = 'ERROR';
+//   }
 
-  $data = array(
-    'error'     =>  $error,
-    'success'   =>  $success
-    );
+//   $data = array(
+//     'error'     =>  $error,
+//     'success'   =>  $success
+//     );
   
-  echo json_encode($data);
-}
+//   echo json_encode($data);
+// }
 
     
 ?>
