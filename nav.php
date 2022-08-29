8c615661dcccf23a3c9f164455e2832d9566c1a0
ekmc16 <112057282+ekmc16@users.noreply.github.com>
2022-08-29 12:33:41 +0800

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,17 +2,24 @@
   <!-- Fixed navbar -->
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">PHP Demo</a>
+      <a class="navbar-brand" href="#">CASE STUDY #2</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/crud">Home</a>
+              <!-- dapat statistics? -->
+              <a class="nav-link" id="dashboard"href="#">Dashboard</a>
+            </li>
+          <li class="nav-item">
+            <a class="nav-link" id="aliens" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/crud/add.php">Add</a>
+            <a class="nav-link" id="bases" href="#">Bases</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="visitors" href="#">Visitors</a>
           </li>
 
         </ul>
