07b83b34ae2bdb48a0f50567538693b692b7292e
ekmc16 <112057282+ekmc16@users.noreply.github.com>
2022-08-29 21:40:21 +0800

    Update fetchinfo.php

--- fetchinfo.php
+++ fetchinfo.php
@@ -2,11 +2,11 @@
 require_once "pdo.php";
 
 if (isset($_GET)){
- if(isset($_GET_['martian_id']))
+ if(isset($_GET_['martian_id'])){
   $stmt = $pdo->prepare("SELECT * FROM martian where martian_id = :id");
   $stmt->execute(array(":id" => $_GET['martian_id']));
   $data = $stmt->fetch(PDO::FETCH_ASSOC);
-
+ }
   echo json_encode($data);
 }
     
