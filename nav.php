2a96be0a126197c93193ce91b0efce7be4eb9966
ekmc16 <112057282+ekmc16@users.noreply.github.com>
2022-08-31 15:49:42 +0800

    Update nav.php

--- nav.php
+++ nav.php
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link" id="martians" href="index.php">Aliens</a>
+            <a class="nav-link" id="martians" href="index.php">Martians</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" id="bases" href="bases.php">Bases</a>
